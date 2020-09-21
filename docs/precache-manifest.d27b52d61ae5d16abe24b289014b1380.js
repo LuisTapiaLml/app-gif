570886a527b29ced784c5c1e520a93b292b4b915
Luis Tapia <luistapialml@gmail.com>
2020-09-20 21:43:18 -0500

    con docs

---
+++ docs/precache-manifest.d27b52d61ae5d16abe24b289014b1380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba044187b192f1df0bdb92c3520e535e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b19a43a3d6ed21e9c44",
+    "url": "/static/css/main.d9f7a755.chunk.css"
+  },
+  {
+    "revision": "ef7fe5dcd6fe2f866af2",
+    "url": "/static/js/2.c555a145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c555a145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b19a43a3d6ed21e9c44",
+    "url": "/static/js/main.cfb02afe.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
